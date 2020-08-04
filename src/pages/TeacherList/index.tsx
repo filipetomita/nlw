841260cb373861e2b0a5ece1127d02d7bfc35595
Filipe Tomita <filipe.tomita@gmail.com>
2020-08-04 00:59:35 -0300

    [ Aula 01 ] 1:59

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+/* CSS */
+import './style.css';
+function TeacherList() {
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Esses são os proffys disponíveis." />
+        </div>
+    )
+}
+
+export default TeacherList;
